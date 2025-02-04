369722dc1d957bc22a76b3466ee44e522ccc4cad
evil666in <158379561+evil666in@users.noreply.github.com>
2025-02-03 21:43:21 -0600

    Add files via upload

---
+++ EVA1_4_COMPARAR_CADENAS/src/eva1_4_comparar_cadenas/EVA1_4_COMPARAR_CADENAS.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_4_comparar_cadenas;
+
+/**
+ *
+ * @author evely
+ */
+public class EVA1_4_COMPARAR_CADENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         String cade1 = "hola";
+        String cade2 = "hola";
+        if(cade1 == cade2)
+            System.out.println("Cadenas iguales");
+        else 
+            System.out.println("Cadenas diferentes");
+    }
+    
+}
